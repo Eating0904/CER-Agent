4fd78042dac3f4639dd6090d191d500516ed7cd3
Eating <alien1128love@gmail.com>
2025-11-12 14:50:49 +0800

    feat: 調整整體布局

--- frontend/src/pages/Template.jsx
+++ frontend/src/pages/Template.jsx
@@ -6,12 +6,13 @@ const { Header, Content } = Layout;
 const { Title } = Typography;
 
 export const Template = ({ children }) => (
-    <Layout>
+    <Layout style={{ height: '100vh', overflow: 'hidden' }}>
         <Header
             style={{
                 display: 'flex',
                 alignItems: 'center',
                 height: '50px',
+                flexShrink: 0,
             }}
         >
             <Link to="/">
@@ -28,9 +29,11 @@ export const Template = ({ children }) => (
         </Header>
         <Content
             style={{
-                padding: '0 24px',
+                padding: '0 24px 24px 24px',
                 height: 'calc(100vh - 50px)',
                 backgroundColor: '#fff',
+                overflowY: 'auto',
+                overflowX: 'hidden',
             }}
         >
             {children}
