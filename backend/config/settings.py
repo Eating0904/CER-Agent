029434cf634cb9ad35731acfbcfa44b3a983f1e8
Eating <alien1128love@gmail.com>
2025-12-22 00:33:26 +0800

    DATABASE_URL 改由 settings 組裝

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -111,6 +111,7 @@ DATABASES = {
     }
 }
 
+DATABASE_URL = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
 
 # User model
 AUTH_USER_MODEL = 'user.User'
